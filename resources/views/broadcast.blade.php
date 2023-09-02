dc9c385c56177561243bc3f3a7ace054eb6b543f
Anuj Shashimal <43109186+anujshashimal@users.noreply.github.com>
2023-09-02 23:42:31 +0530

    intial commit

---
+++ resources/views/broadcast.blade.php
@@ -0,0 +1,4 @@
+<div class="right message">
+  <p>{{$message}}</p>
+  <img src="https://img.icons8.com/color/48/brave.png" alt="brave"/>
+</div>
---
+++ resources/views/receive.blade.php
@@ -0,0 +1,4 @@
+<div class="left message">
+  <img src="https://img.icons8.com/doodle/48/user.png" alt="Avatar"/>
+  <p>{{$message}}</p>
+</div>
